d5023903c44fa9c7295f53dc5d641111c3c6396d
viclomaro <viclomaro@gmail.com>
2020-03-10 18:45:02 +0100

    Echas todas las querys menos la de tecnicas

--- appFotografia/ServidorFotografia/models/producto.js
+++ appFotografia/ServidorFotografia/models/producto.js
@@ -1,12 +1,76 @@
+const getAll = () => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from productos', (err, rows) => {
+            if (err) reject(err);
+            resolve(rows);
+        })
+    });
+};
+
 const getByCategoria = (pCategoria) => {
     return new Promise((resolve, reject) => {
-        db.query('select * from productos where categoria = ?', [pCategoria],
-            (err, rows) => {
-                if (err) reject(err);
-                if (rows.length === 0) {
-                    resolve(null)
-                }
-                resolve(rows[rows.length]);
-            })
+        db.query('select * from productos where categoria = ?', [pCategoria], (err, rows) => {
+            if (err) reject(err);
+            if (rows.length === 0) {
+                resolve(null);
+            }
+            resolve(rows);
+        })
     });
+}
+
+const getByResolucion = (pResolucionMin, pResolucionMax) => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from productos where resolucion > ? and resolucion < ?', [pResolucionMin, pResolucionMax], (err, rows) => {
+            if (rows.length === 0) {
+                resolve(null);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+const getByFocal = (pFocalMin, pFocalMax) => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from productos where focal > ? and focal < ?', [pFocalMin, pFocalMax], (err, rows) => {
+            if (rows.length === 0) {
+                resolve(null);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+const getByIso = (pIsoMin, pIsoMax) => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from productos where iso > ? and iso < ?', [pIsoMin, pIsoMax], (err, rows) => {
+            if (rows.length === 0) {
+                resolve(null);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+
+const getByPrecio = (pPrecioMin, pPrecioMax) => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from productos where precio > ? and precio < ?', [pPrecioMin, pPrecioMax], (err, rows) => {
+            if (rows.length === 0) {
+                resolve(null);
+            }
+            resolve(rows);
+        })
+    })
+}
+
+
+
+module.exports = {
+    getByCategoria: getByCategoria,
+    getAll: getAll,
+    getByResolucion: getByResolucion,
+    getByFocal: getByFocal,
+    getByIso: getByIso,
+    getByPrecio: getByPrecio
 }
--- appFotografia/ServidorFotografia/routes/api/productos.js
+++ appFotografia/ServidorFotografia/routes/api/productos.js
@@ -1,14 +1,55 @@
 const router = require('express').Router();
+const Producto = require('../../models/producto');
+
+
+// GET http://localhost:3000/api/productos
+router.get('/', async (req, res) => {
+    const productos = await Producto.getAll();
+    res.json(productos)
+})
+
 
 
 //GET http://localhost:3000/api/productos/:categoria
+router.get('/:categoria', async (req, res) => {
+    const productos = await Producto.getByCategoria(req.params['categoria']);
+    res.json(productos)
+})
 
-/*
-router.get('/:categoria', async(req, res) => {
-            const rows = await producto.getByCategoria(req, res) =>{
-                res.json(result)
-            }
-            });
 
-            */
+
+// GET http://localhost:3000/api/productos/resolucion/resolucionMin/resolucionMax
+router.get('/resolucion/:resolucionMin/:resolucionMax', async (req, res) => {
+    const productos = await Producto.getByResolucion(req.params.resolucionMin, req.params.resolucionMax)
+    res.json(productos)
+})
+
+
+
+// GET http://localhost:3000/api/productos/focal/focalMin/focalMax
+router.get('/focal/:focalMin/:focalMax', async (req, res) => {
+    const productos = await Producto.getByFocal(req.params.focalMin, req.params.focalMax);
+    res.json(productos)
+})
+
+
+
+// GET http://localhost:3000/api/productos/iso/isoMin/isoMax
+router.get('/iso/:isoMin/:isoMax', async (req, res) => {
+    const productos = await Producto.getByIso(req.params.isoMin, req.params.isoMax);
+    res.json(productos)
+})
+
+
+
+
+//GET http://localhost:3000/api/productos/precio/precioMin/precioMax
+router.get('/precio/:precioMin/:precioMax', async(req, res)=>{
+    const productos = await Producto.getByPrecio(req.params.precioMin, req.params.precioMax);
+    res.json(productos)
+})
+
+
+
+
 module.exports = router;
