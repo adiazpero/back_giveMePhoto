235d6559ab3011a91d1b958ead07a4f3ebf55803
adiazpero <anitta_83@hotmail.com>
2020-03-24 11:30:10 +0100

    query para cursos

---
+++ appFotografia/ServidorFotografia/models/curso.js
@@ -0,0 +1,20 @@
+const getByNivel = (pNivel) => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from cursos where nivel = ?', [pNivel], (err, rows) => {
+            if (err) reject(err);
+            if (rows.length === 0) {
+                resolve(null);
+            }
+            resolve(rows);
+        })
+    });
+}
+
+
+
+
+
+
+module.exports = {
+    getByNivel: getByNivel,
+}
--- appFotografia/ServidorFotografia/routes/api.js
+++ appFotografia/ServidorFotografia/routes/api.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiUsersRouter = require('./api/users');
 const apiProductosRouter = require('./api/productos');
+const apiCursosRouter = require('./api/cursos');
 
 router.use('/users', apiUsersRouter);
 router.use('/productos', apiProductosRouter);
+router.use('/cursos', apiCursosRouter);
 
 module.exports = router;
